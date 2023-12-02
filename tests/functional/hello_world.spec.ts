c5b66fc83ee5443040e2f0956fd26bc05bc308a8
Mahabub <mahabubx7@gmail.com>
2023-12-03 01:59:49 +0600

    setup(test): :zap: inital-test-setup

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@japa/runner'
 
 test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+  const response = await client.get('/api')
 
   response.assertStatus(200)
   response.assertBodyContains({ hello: 'world' })
